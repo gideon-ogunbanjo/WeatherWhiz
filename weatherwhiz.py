d32aed715d9cd2b0cc9c3ae71745e2caf2baef5c
Gideon Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-04-23 20:24:33 +0100

    created a window

--- weatherwhiz.py
+++ weatherwhiz.py
@@ -3,3 +3,14 @@ from tkinter import *
 import requests
 import json
 from datetime import datetime
+
+# initializing the window
+
+root =Tk()
+root.geometry("400x400") # size of the window by default
+root.resizable(0,0) # making the window size fixed
+
+#title of the window
+
+root.title("Weather App - AskPython.com") 
+
