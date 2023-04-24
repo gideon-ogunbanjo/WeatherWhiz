2ec528eb48292512b2da2e4f6cf7fd61d13f1fa1
Gideon Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-04-24 12:02:19 +0100

    fixed bugs

--- weatherwhiz.py
+++ weatherwhiz.py
@@ -62,7 +62,7 @@ def showWeather():
          
         weather = f"\nWeather of: {city_name}\nTemperature (Celsius): {temp}°\nEstimated degree in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time}, and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {description}"
     else:
-        weather = f"\n\tWeather for '{city_name}' not found!\n\tKindly Enter valid Country Name !!"
+        weather = f"\n\tWeather for '{city_name}' not found!\n\tKindly Enter valid Location Name !!"
  
     tfield.insert(INSERT, weather)   # used to insert or send value in our Text Field to display output
 
